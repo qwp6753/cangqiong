4866750e0888e40965f8a60a858abe3c2694b522
qwp6753 <3084474884@qq.com>
2026-07-19 16:48:47 +0800

    新增员工业务代码开发

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,6 +1,7 @@
 package com.sky.mapper;
 
 import com.sky.entity.Employee;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -15,4 +16,13 @@ public interface EmployeeMapper {
     @Select("select * from employee where username = #{username}")
     Employee getByUsername(String username);
 
+    /**
+     * 插入员工数据
+     * @param employee
+     */
+    //language=MySQL
+    @Insert("insert into sky_take_out.employee(employee.name, employee.username, employee.password, employee.phone, employee.sex, employee.id_number, employee.status, employee.create_time, employee.update_time, employee.create_user, employee.update_user) " +
+            "values " +
+            "(#{name},#{username},#{password},#{phone},#{sex},#{idNumber},#{status},#{createTime},#{updateTime},#{createUser},#{updateUser})")
+    void insert(Employee employee);
 }
