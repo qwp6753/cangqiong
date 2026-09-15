6a0bb0c562d5e5cd6aac809b167602c728816111
qwp6753 <3084474884@qq.com>
2026-09-15 18:35:52 +0800

    编辑员工消息业务代码开发

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -35,4 +35,17 @@ public interface EmployeeService {
      * @param status
      */
     void startOrStop(Integer status, Long id);
+
+    /**
+     * 根据id查询员工信息
+     * @param id
+     * @return
+     */
+    Employee getById(Long id);
+
+    /**
+     * 编辑员工信息
+     * @param employeeDTO
+     */
+    void update(EmployeeDTO employeeDTO);
 }
